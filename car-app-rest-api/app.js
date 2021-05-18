802ce27544bf531c2701d5c5b1f6f92e274a00dd
Syed Minhal Nadeem <minhalnadeem@gmail.com>
2021-05-18 09:50:59 +0500

    Cors module added

--- car-app-rest-api/app.js
+++ car-app-rest-api/app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route');
 const app = express();
+var cors = require('cors')
 
+app.use(cors());
+app.options('*', cors())
 app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.json({ limit: '10mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
