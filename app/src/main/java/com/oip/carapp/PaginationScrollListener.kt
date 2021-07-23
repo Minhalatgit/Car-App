a72b7615defada986bdef9f6ed2f83fa02806af2
Syed Minhal Nadeem <minhalnadeem@gmail.com>
2021-07-23 17:41:21 +0500

    Multpi imaeg upload in progress

--- app/src/main/java/com/oip/carapp/PaginationScrollListener.kt
+++ app/src/main/java/com/oip/carapp/PaginationScrollListener.kt
@@ -1,6 +1,5 @@
 package com.oip.carapp
 
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
